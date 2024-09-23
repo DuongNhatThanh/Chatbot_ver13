629d4073fc15c40712601e3dd21dadb9c43c108c
PhamTrinhDuc <duc78240@gmail.com>
2024-09-24 00:22:45 +0700

    update 24/9 12:22

--- module_elastic/__init__.py
+++ module_elastic/__init__.py
@@ -1,4 +1,3 @@
-from .init_client import init_elastic
-from .elastic_helper import parse_specification_range, get_keywords, find_closest_match
+from .elastic_helper import parse_specification_range, get_keywords, find_closest_match, init_elastic
 from .query_engine import search_db
 from .few_shot_sentence import classify_intent
